fac047a8b88177f80f5819dfbe66a8f1c3e9b685
CHUSUEYEON <rmffkfk226@naver.com>
2024-08-13 22:18:56 +0900

    feat:[BE] UserController 파일 생성

---
+++ src/main/java/Try_it/user/UserController.java
@@ -0,0 +1,9 @@
+package Try_it.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+}
