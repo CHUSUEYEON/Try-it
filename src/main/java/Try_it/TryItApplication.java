dd5ca003a83169b96eba5e9a19a1d88ca6252a0b
CHUSUEYEON <rmffkfk226@naver.com>
2024-08-13 22:01:34 +0900

    chore:[BE]깃연동

---
+++ src/main/java/Try_it/TryItApplication.java
@@ -0,0 +1,13 @@
+package Try_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Try_it/TryItApplicationTests.java
@@ -0,0 +1,13 @@
+package Try_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
